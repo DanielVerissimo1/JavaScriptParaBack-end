6bd88d513690ee512a04e0fa3c00ed71732d0949
DanielVerissimo1 <danielSantoss1300@gmail.com>
2025-05-04 21:58:43 -0300

    Dando os primeiros passos

---
+++ index.js
@@ -0,0 +1,6 @@
+const PromptSync = require("prompt-sync")();
+
+
+const nota1 = PromptSync("fala pra tu ver: ")
+// nota1 = +PromptSync("digite um numero: ")  o + serve para mudar de string para int
+console.log(nota1)
